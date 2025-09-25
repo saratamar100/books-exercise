2adff73a605d8231211d2839bd4a31f7569be2bb
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-09-25 14:56:47 +0300

    logger to file

--- looger.js
+++ looger.js
@@ -1,6 +1,14 @@
-//middleware for logging
+const path = require("path");
+const fs = require("fs");
+const logPath = path.join(__dirname, "log.txt");
+
 function logger(req, res, next) {
-  console.log("a call was made");
+  console.log("a call eas made");
+  fs.appendFile(logPath, "a call was made\n", (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
   next();
 }
 
